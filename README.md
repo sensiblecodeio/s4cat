2b9d66b6963daade6f3711cdfe5f01cea0248acf
Peter Waller <peter@scraperwiki.com>
2014-12-16 10:38:27 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Simple S3 cat program
+=====================
+
+Super simple program to fetch objects from S3 using credentials from an
+instance's role.
+
+Usage:
+
+```
+s4cat <bucket name> <key>
+```
---
+++ main.go
@@ -0,0 +1,35 @@
+// Note: doesn't work yet.
+// Note: you may wish to modify the hard-coded region from eu-west-1.
+
+package main
+
+import (
+	"io"
+	"log"
+	"os"
+
+	"github.com/stripe/aws-go/aws"
+	"github.com/stripe/aws-go/gen/s3"
+)
+
+func main() {
+	if len(os.Args) < 3 {
+		log.Fatal("usage: s4cat <bucket> <key>")
+	}
+	bucket, key := os.Args[1], os.Args[2]
+
+	creds := aws.IAMCreds()
+
+	s := s3.New(creds, "eu-west-1", nil)
+	resp, err := s.GetObject(&s3.GetObjectRequest{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	})
+	if err != nil {
+		log.Fatalf("GetObject Failed: %#+v", err)
+	}
+	n, err := io.Copy(os.Stdout, resp.Body)
+	if err != nil {
+		log.Fatal("Copy failed after", n, "bytes:", err)
+	}
+}
